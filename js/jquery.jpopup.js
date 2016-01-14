c525bbb8b5d5ec074f1f8af1e5c71a2590827b7c
Thomas Gladdines <seahorsepip@outlook.com>
2016-01-14 22:36:01 +0100

    Small charachter encode bug fix

--- js/jquery.jpopup.js
+++ js/jquery.jpopup.js
@@ -17,7 +17,7 @@ function jPopup(config) {
 		scrollTopClass: classBase+"_scroll_top",
 		scrollBottomClass: classBase+"_scroll_bottom",
 		closeButtonClass: classBase+"_close",
-		closeButtonContent: "×<svg fill=\"#000000\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z\"/></svg>",
+		closeButtonContent: "&times;<svg fill=\"#000000\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z\"/></svg>",
 		closeButton: false,
 		overlay: true,
 		overlayClose: false,
