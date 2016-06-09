b8ec6946d6a96c0905215cd92c9823afc154e40f
Thomas Gladdines <seahorsepip@outlook.com>
2016-06-09 17:39:19 +0200

    Removed redundant top variable.

--- js/jquery.jpopup.js
+++ js/jquery.jpopup.js
@@ -49,8 +49,7 @@ function jPopup(config) {
 	var popupResize = $("<div class=\""+config["resizeClass"]+"\" style=\"display: none;\"><div style=\"position:absolute;top:0;left:0;right: 0;height:6px;cursor:n-resize;\"></div><div style=\"position:absolute;top:0;left:0;bottom:0;width:6px;cursor:w-resize;\"></div><div style=\"position:absolute;left:0;bottom:0;right:0;height:6px;cursor:s-resize;\"></div><div style=\"position:absolute;top:0;bottom:0;right:0;width:6px;cursor:e-resize;\"></div><div style=\"position:absolute;top:0;left:0;width:6px;height:6px;cursor:nw-resize;\"></div><div style=\"position:absolute;top:0;right:0;width:6px;height:6px;cursor:ne-resize;\"></div><div style=\"position:absolute;left:0;bottom:0;width:6px;height:6px;cursor:sw-resize;\"></div><div style=\"position:absolute;bottom:0;right:0;width:6px;height:6px;cursor:se-resize;\"></div></div>");
 	console.log(popupWrapper);
 	popup.append(popupResize);
-	var top,
-		fY,
+	var fY,
 		fX,
 		rY,
 		rX,
@@ -65,7 +64,7 @@ function jPopup(config) {
 
 	function freeze() {
 		if($("html").css("position") != "fixed" && config["overlay"]) {
-			top = $("html").scrollTop() ? $("html").scrollTop() : $("body").scrollTop();
+			var top = $("html").scrollTop() ? $("html").scrollTop() : $("body").scrollTop();
 			if(window.innerWidth > $("html").width()) {
 				$("html").css("overflow-y", "scroll");
 			}
